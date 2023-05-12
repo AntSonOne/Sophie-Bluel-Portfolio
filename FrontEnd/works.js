1e9e91d84c06fba9b59c008f8c3a7f993c821ca0
Antoine Berson <antoine.berson01@gmail.com>
2023-05-12 11:33:46 +0200

    wip

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,1 +1,22 @@
-fetch("http://localhost:5678/api/works");
+const reponse = await fetch("http://localhost:5678/api/works");
+const works = await reponse.json();
+
+function genererWorks(works){
+  for (let i = 0; i < works.length; i++) {
+
+    const article = works[i]
+    const sectionGallery = document.querySelector(".gallery");
+    const workElement = document.createElement("figure");
+    workElement.dataset.id = works[i].id;
+    const imageElement = document.createElement("img");
+    imageElement.src = article.imageUrl;
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = article.title;
+
+    sectionGallery.appendChild(workElement);
+    workElement.appendChild(imageElement);
+    workElement.appendChild(titleElement);
+  }
+}
+
+genererWorks(works);
