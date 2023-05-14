45b8fa5a26504095f67d077ae1387b0e1ec15766
Antoine Berson <antoine.berson01@gmail.com>
2023-05-14 12:38:14 +0200

    login works, need token storage

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,44 +1,27 @@
-console.log('hello')
-
-function ajoutListenerEnvoyerLogin() {
+function ajoutListenerLogin() {
   const formulaireLogin = document.querySelector(".login");
-  formulaireLogin.addEventListener("submit", function (event) {
+  formulaireLogin.addEventListener("submit", async function (event) {
   event.preventDefault();
 
-  const login = {
-      pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-      utilisateur: event.target.querySelector("[name=utilisateur]").value,
-      commentaire: event.target.querySelector("[name=commentaire]").value,
-      nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
-  };
+    const login = {
+      email: event.target.querySelector("[name=email]").value,
+      password: event.target.querySelector("[name=password]").value,
+    };
 
-  const chargeUtile = JSON.stringify(login);
+    const chargeUtile = JSON.stringify(login);
 
-  fetch("http://localhost:5678/api/users/login", {
+    const reponse = await fetch("http://localhost:5678/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: chargeUtile
-  });
-  });
-}
+    });
 
-function login() {
-  const formulaireLogin = document.querySelector(".login");
-  formulaireLogin.addEventListener("submit", function (event) {
-    if (condition) {
-      return "Erreur dans l’identifiant ou le mot de passe"
+    if (reponse.status !== 200) {
+      alert("Erreur dans l’identifiant ou le mot de passe");
     } else {
-      // redirect home page as user
+      location.replace("index.html");
     }
-  })
-}
+  });
+};
 
-// fetch("/colis/livraison", {
-
-//   "method": "POST",
-
-//   "headers": { "Content-Type": "application/json" },
-
-//   "body": /* ... */
-
-// });
+ajoutListenerLogin()
