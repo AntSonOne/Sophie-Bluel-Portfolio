3cf8e2c76d9e857d190b5e7fbf3b82f9da319611
Antoine Berson <antoine.berson01@gmail.com>
2023-05-26 14:17:42 +0200

    delete work ok, NEED maintain modal open

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,10 +1,10 @@
-const TOKEN = window.localStorage.getItem( 'token' );
-const TOKENLENGTH = 143
+const TOKEN = window.localStorage.getItem("token");
+const TOKENLENGTH = 143;
 
 function ajoutListenerLogin() {
   const formulaireLogin = document.querySelector(".login");
-  formulaireLogin.addEventListener("submit", async function(event) {
-  event.preventDefault();
+  formulaireLogin.addEventListener("submit", async function (event) {
+    event.preventDefault();
 
     const login = {
       email: event.target.querySelector("[name=email]").value,
@@ -16,26 +16,28 @@ function ajoutListenerLogin() {
     const reponse = await fetch("http://localhost:5678/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: chargeUtile
+      body: chargeUtile,
     })
-    .then(response => response.json())
-    .then(function(data){
-      const accessToken = data.token;
-      if (accessToken && accessToken.length === TOKENLENGTH){
-        window.localStorage.setItem( 'token', accessToken );
-        location.replace("user.html");
-      } else {
-        alert("Erreur dans l’identifiant ou le mot de passe");
-      }
-
-    });
+      .then((response) => response.json())
+      .then(function (data) {
+        const accessToken = data.token;
+        if (accessToken && accessToken.length === TOKENLENGTH) {
+          window.localStorage.setItem("token", accessToken);
+          location.replace("user.html");
+        } else {
+          alert("Erreur dans l’identifiant ou le mot de passe");
+        }
+      });
   });
-};
+}
 
-ajoutListenerLogin()
+ajoutListenerLogin();
 
-console.log(TOKEN.length, TOKENLENGTH)
+console.log(TOKEN.length, TOKENLENGTH);
 
-if (window.location.pathname.includes('user.html') && TOKEN.length !== TOKENLENGTH) {
+if (
+  window.location.pathname.includes("user.html") &&
+  TOKEN.length !== TOKENLENGTH
+) {
   location.replace("index.html");
 }
